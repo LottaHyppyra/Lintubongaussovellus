136fc642e767a2e172a6a28fe3c66cfb8146f381
Lotta Hyppyrä <lotta@hyppyra.net>
2023-04-01 13:05:58 +0300

    Create sightings

---
+++ sightings.py
@@ -0,0 +1,18 @@
+from db import db
+from sqlalchemy.sql import text
+
+def get_all():
+    sql = text("SELECT species, location, date FROM sightings")
+    return db.session.execute(sql).fetchall()
+
+def get_from_user(user_id):
+    sql = text("SELECT species, location, date FROM sightings WHERE user_id=:user_id")
+    return db.session.execute(sql, {"user_id":user_id}).fetchall()
+
+def add(species, location, date, user_id):
+    try:
+        sql = text("INSERT INTO sightings (species, location, date, user_id) VALUES (:species, :location, :date, :user_id)")
+        db.session.execute(sql, {"species":species, "location":location, "date":date, "user_id":user_id})
+        db.session.commit()
+    except:
+        return False
