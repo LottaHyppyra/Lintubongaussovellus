e3a3347af71154e7f2d363d548bb0581f87539a6
Lotta Hyppyrä <lotta@hyppyra.net>
2023-04-01 12:46:24 +0300

    Add sightings

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     password TEXT,
     sightings INTEGER
 );
+
+CREATE TABLE sightings (
+    id SERIAL PRIMARY KEY,
+    species TEXT,
+    location TEXT,
+    date DATE,
+    user_id INTEGER REFERENCES users
+);
