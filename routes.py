2bd412aecc6a68b04f11921f10d08809ac6d51c0
Lotta Hyppyrä <lotta@hyppyra.net>
2023-04-01 12:31:50 +0300

    Add registeration and login possibility

--- routes.py
+++ routes.py
@@ -1,6 +1,47 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+
+        if not users.login(name, password):
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+        return redirect("/")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods =["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        name = request.form["name"]
+        if len(name) < 1 or len(name) > 20:
+            return render_template("error.html", message="Tunnuksen tulee olla 1-20 merkkiä pitkä")
+
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if password1 == "":
+            return render_template("error.html", message="Salasana on tyhjä")
+
+        if not users.register(name, password1):
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
+
+        return redirect("/")
