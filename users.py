08af58e659a3b9025613760bb29ca07860a9b389
Lotta Hyppyrä <lotta@hyppyra.net>
2023-04-01 10:36:10 +0300

    Create users.py

---
+++ users.py
@@ -0,0 +1,16 @@
+import os
+from flask import abort, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
+
+def register(kayttajatunnus, salasana):
+    return
+
+def login(kayttajatunnus, salasana):
+    return
+
+def logout():
+    return
+
+def user_id():
+    return
